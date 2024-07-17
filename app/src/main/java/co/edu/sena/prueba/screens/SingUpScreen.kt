4910884fedfe8b5fb89e4a503dc5fb24acef5ce9
jdhernandez1832 <jdhernandez1832@soy.sena.edu.co>
2024-07-17 17:27:34 -0500

    se agrego nuevas carpetas con sus respectivos componentes

--- app/src/main/java/co/edu/sena/prueba/screens/SingUpScreen.kt
+++ app/src/main/java/co/edu/sena/prueba/screens/SingUpScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.prueba.R
 import co.edu.sena.prueba.components.NormalTextComponent
 
 @Composable
@@ -14,7 +16,7 @@ fun SingUpScreen() {
         color = Color.White,
         modifier = Modifier.fillMaxSize()
     ) {
-        NormalTextComponent(value = "Hey there")
+        NormalTextComponent(value = stringResource(id = R.string.hello))
     }//Surface
 }//SignUpScreen
 
