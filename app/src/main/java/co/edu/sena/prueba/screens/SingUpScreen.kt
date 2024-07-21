e0fe9b4e0378c702eeb5775de850beb57b494134
jdhernandez1832 <jdhernandez1832@soy.sena.edu.co>
2024-07-21 18:51:03 -0500

    feature: se agrega un Column y sus respectivos contenidos asi como los iconos

--- app/src/main/java/co/edu/sena/prueba/screens/SingUpScreen.kt
+++ app/src/main/java/co/edu/sena/prueba/screens/SingUpScreen.kt
@@ -1,22 +1,50 @@
 package co.edu.sena.prueba.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import co.edu.sena.prueba.R
+import co.edu.sena.prueba.components.HeadingTextComponent
+import co.edu.sena.prueba.components.MyTextField
 import co.edu.sena.prueba.components.NormalTextComponent
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SingUpScreen() {
     Surface(
-        color = Color.White,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
     ) {
-        NormalTextComponent(value = stringResource(id = R.string.hello))
+        Column (modifier= Modifier.fillMaxSize().background(Color.White)){
+            NormalTextComponent(value = stringResource(id = R.string.hello))
+            HeadingTextComponent(value = stringResource(id = R.string.create_account))
+            Spacer(modifier=Modifier.height(20.dp))
+            MyTextField(
+                labelValue = stringResource(id = R.string.first_name),
+                painterResource(id = R.drawable.profile))
+            MyTextField(
+                labelValue = stringResource(id = R.string.last_name),
+                painterResource = painterResource(id = R.drawable.profile)
+            )
+            MyTextField(
+                labelValue = stringResource(id = R.string.email),
+                painterResource = painterResource(id = R.drawable.message)
+            )
+        }//Column
     }//Surface
 }//SignUpScreen
 
