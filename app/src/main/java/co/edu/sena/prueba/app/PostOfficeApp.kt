be95d9e3dc5213e1052aa4a5d4de840c4a443f7a
jdhernandez1832 <jdhernandez1832@soy.sena.edu.co>
2024-07-21 18:48:08 -0500

    featurd: se agrega la funcion SingUp para ejecutar en el dispositivo

--- app/src/main/java/co/edu/sena/prueba/app/PostOfficeApp.kt
+++ app/src/main/java/co/edu/sena/prueba/app/PostOfficeApp.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import co.edu.sena.prueba.screens.SingUpScreen
 
 @Composable
 fun PostOfficeApp() {
@@ -12,6 +13,6 @@ fun PostOfficeApp() {
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ) {
-        
+        SingUpScreen()
     }
 }
